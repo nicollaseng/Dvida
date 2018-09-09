fc41b0329981c9b2951c4638d05668d9a4d45e0a
Nicollas Matheus <n.matheuscarvalho@gmail.com>
2018-09-09 10:13:08 -0300

    first commit

---
+++ src/EntryPointApp.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+import LoginScreen from './components/LoginScreen'
+
+export default class EntryPointApp extends Component {
+  render() {
+    return (
+      <LoginScreen />
+    );
+  }
+}
+
+
---
+++ src/components/LoginScreen.js
@@ -0,0 +1,17 @@
+import React,{Component} from 'react'
+
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Container, Label} from 'native-base'
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <Container>
+          <Label>Olá</Label>
+      </Container>
+    );
+  }
+}
+
+
