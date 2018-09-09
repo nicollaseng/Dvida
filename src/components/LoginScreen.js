3c66cf378dbe1c9d0e8ab8ae461c9b05239c01d3
Nicollas Matheus <n.matheuscarvalho@gmail.com>
2018-09-09 10:34:46 -0300

    LoginScreen inputs created

--- src/components/LoginScreen.js
+++ src/components/LoginScreen.js
@@ -1,17 +1,74 @@
 import React,{Component} from 'react'
 
-import {Platform, StyleSheet, Text, View} from 'react-native';
-import {Container, Label} from 'native-base'
+import {Platform, TextInput, StyleSheet} from 'react-native';
+import {Container, Text, Form, Input, Item, Button} from 'native-base'
 
 
 export default class App extends Component {
   render() {
     return (
-      <Container>
-          <Label>Olá</Label>
+      <Container style={mainContainer}>
+        <Form style={logoView}>
+            <Text style={logoStyle}>D.Vida</Text>
+            <Text style={sloganStyle}>ReactNative Test</Text>
+        </Form>
+        <Form style={inputsView}>
+            <TextInput placeholder='Email Adress' style={inputEmailStyle}/>
+            <TextInput placeholder='Passwrod' style={inputPasswordStyle}/>
+        </Form>
+        <Form style={buttonView}>
+            <Button>
+                <Text>Login</Text>
+            </Button>
+        </Form>
+          
       </Container>
     );
   }
 }
 
 
+const styles = StyleSheet.create({
+    mainContainer: {
+        flex: 1
+    },
+    logoView: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    logoStyle: {
+        fontSize: 40,
+        fontWeight: 'bold'
+    },
+    sloganStyle:{
+        fontSize: 16,
+        color: '#ccc',
+    },
+    inputsView:{
+        flex: 2,
+        height: 30
+    },
+    inputEmailStyle:{
+        height: 50,
+        borderWidth: 1,
+        borderColor: '#b3b3b3',
+        borderRadius: 3,
+        maxWidth: 350,
+        marginLeft: 10,
+    },
+    inputPasswordStyle:{
+        height: 50,
+        borderWidth: 1,
+        borderColor: '#b3b3b3',
+        borderRadius: 3,
+        maxWidth: 350,
+        marginLeft: 10
+    },
+    buttonView: {
+        flex: 1
+    }
+})
+
+const {mainContainer, logoView, inputsView, buttonView,
+        inputEmailStyle, logoStyle, sloganStyle, inputPasswordStyle} = styles
